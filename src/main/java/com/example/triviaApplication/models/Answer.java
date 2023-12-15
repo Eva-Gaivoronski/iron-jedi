99a4ca2856237f88572b4094bb2bde25755f6380
Kevin <cfphpl@gmail.com>
2023-12-15 12:28:15 -0600

    fixed import issue in answers model

--- src/main/java/com/example/triviaApplication/models/Answer.java
+++ src/main/java/com/example/triviaApplication/models/Answer.java
@@ -5,10 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 public class Answer {
