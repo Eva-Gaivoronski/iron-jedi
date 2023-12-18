9a23778aaf7e1d29d3580d822e8053f652e4d756
Kevin <cfphpl@gmail.com>
2023-12-17 21:56:20 -0600

    Connecting react to spring

--- src/main/java/com/example/triviaApplication/controllers/QuestionController.java
+++ src/main/java/com/example/triviaApplication/controllers/QuestionController.java
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/questions")
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionRepository questionRepository;
--- src/main/java/com/example/triviaApplication/models/Question.java
+++ src/main/java/com/example/triviaApplication/models/Question.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 
 @Entity
 public class Question {
