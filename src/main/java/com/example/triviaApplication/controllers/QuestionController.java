4307a049ddc2bf6e733af835fcf0eb4c75ca67d3
Kevin <cfphpl@gmail.com>
2023-12-17 19:06:49 -0600

    react connect code

--- src/main/java/com/example/triviaApplication/controllers/QuestionController.java
+++ src/main/java/com/example/triviaApplication/controllers/QuestionController.java
@@ -1,26 +1,42 @@
 package com.example.triviaApplication.controllers;
 
 import com.example.triviaApplication.models.Question;
+import com.example.triviaApplication.models.User;
 import com.example.triviaApplication.repositories.QuestionRepository;
+import com.example.triviaApplication.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+    private final UserRepository userRepository;
+
     @Autowired
-    private QuestionRepository questionRepository;
-
+    public QuestionController(QuestionRepository questionRepository, UserRepository userRepository) {
+        this.questionRepository = questionRepository;
+        this.userRepository =userRepository;
+    }
     @PostMapping
-    public Question createQuestion(@RequestBody Question question) {
-
-        return questionRepository.save(question);
+    public ResponseEntity<Question> createQuestion(@RequestBody Question question) {
+        if (question.getUser() != null && question.getUser().getUsername() != null) {
+        User user = userRepository.findByUsername(question.getUser().getUsername())
+        .orElseThrow(() -> new RuntimeException("User not found"));
+        question.setUser(user);
+        }
+        Question savedQuestion = questionRepository.save(question);
+        return ResponseEntity.ok(savedQuestion);
     }
 
     @GetMapping("/{id}")
-    public Question getQuestion(@PathVariable Long id) {
-
-        return questionRepository.findById(id).orElse(null);
+    public ResponseEntity<Question> getQuestion(@PathVariable Long id) {
+        Question question = questionRepository.findById(id)
+        .orElse(null);
+        return question != null ? ResponseEntity.ok(question) : ResponseEntity.notFound().build();
     }
+
+
 }
