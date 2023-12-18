730e2540b5aebed9ed8edb6b34a065c83cdfd790
temesgenghidey <tghidey@gmail.com>
2023-12-18 05:12:50 -0600

    put basic foundation for user classes, need to debug and modify

--- src/main/java/com/example/triviaApplication/Repository/UserRepository.java
+++ src/main/java/com/example/triviaApplication/Repository/UserRepository.java
@@ -1,4 +1,7 @@
-package com.example.triviaApplication.Repository;
+package com.example.triviaApplication;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
--- src/main/java/com/example/triviaApplication/Service/UserService.java
+++ src/main/java/com/example/triviaApplication/Service/UserService.java
@@ -1,4 +1,17 @@
-package com.example.triviaApplication.Service;
+package com.example.triviaApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Method to create a user, send verification email, etc.
 }
