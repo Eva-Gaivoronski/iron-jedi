e11fffb708ff3fcef6bcc829601c6f81043b2e49
allenjosh1983 <138170235+allenjosh1983@users.noreply.github.com>
2023-12-18 13:02:43 -0600

    display json in console

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
-  return (
+
+const[quiz, setQuiz] = useState([]);
+
+useEffect(() => {
+//if (typeof window !== 'undefined') {
+   //getQuiz();
+//}
+//async function getQuiz() {
+//const api = await fetch('https://opentdb.com/api.php?amount=1');
+//const apijson =await api.json();
+//setQuiz(apijson);
+//}
+// }, []);
+const fetchQuiz = async () => {
+      try {
+        const response = await fetch('https://opentdb.com/api.php?amount=1');
+        const data = await response.json();
+        console.log(data); // Assuming the API response has a 'results' property
+      } catch (error) {
+        console.log('Error fetching quiz:', error);
+      }
+    };
+
+    fetchQuiz();
+  }, []);
+
+return(
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +44,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <p> </p>
         </a>
       </header>
     </div>
