695bb865bd06ee084996a8449f70475bd4ce857f
temesgenghidey <tghidey@gmail.com>
2023-12-18 05:04:52 -0600

    1st commit

--- src/main/java/com/example/triviaApplication/Controllers/User.java
+++ src/main/java/com/example/triviaApplication/Controllers/User.java
@@ -1,4 +1,31 @@
 package com.example.triviaApplication.Controllers;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class User {
+    package com.example.triviaApplication;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+    @Entity
+    public class User {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private Long id;
+        private String username;
+        private String password; // Should be hashed
+        private String email;
+        private boolean isVerified;
+        private String profilePictureUrl;
+
+        // Constructors, getters, and setters...
+    }
+
 }
