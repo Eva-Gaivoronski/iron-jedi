381ed03b6d483bf63f2a5615883e9e00a4ae6af1
temesgenghidey <tghidey@gmail.com>
2023-12-18 18:02:57 -0600

    commit

--- src/main/java/com/example/triviaApplication/Controllers/AccountController.java
+++ src/main/java/com/example/triviaApplication/Controllers/AccountController.java
@@ -1,4 +1,18 @@
-package com.example.triviaApplication.Controllers;
+package com.example.triviaApplication;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/account")
 public class AccountController {
+
+    private final UserService userService;
+
+    @Autowired
+    public AccountController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // Endpoints for creating an account, verifying, and changing profile pictures
 }
