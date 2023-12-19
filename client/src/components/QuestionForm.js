8f6d6e747aa64b658d31418ab4a7f3b2cf083562
Kevin <cfphpl@gmail.com>
2023-12-19 08:51:44 -0600

    Added search to question page

--- client/src/components/QuestionForm.js
+++ client/src/components/QuestionForm.js
@@ -7,6 +7,8 @@ function QuestionForm() {
     const [option1, setOption1] = useState('');
     const [option2, setOption2] = useState('');
     const [option3, setOption3] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -36,7 +38,7 @@ function QuestionForm() {
             const responseData = await response.json();
             console.log(responseData);
             alert('Question saved successfully!');
-            gitsetUsername('');
+            setUsername('');
             setQuestionText('');
             setCorrectAnswer('');
             setOption1('');
@@ -48,38 +50,72 @@ function QuestionForm() {
         }
     };
 
-    return (
-        <div>
-            <h2>Create a New Question</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                </div>
-                <div>
-                    <label>Question:</label>
-                    <input type="text" value={questionText} onChange={(e) => setQuestionText(e.target.value)} />
-                </div>
-                <div>
-                    <label>Correct Answer:</label>
-                    <input type="text" value={correctAnswer} onChange={(e) => setCorrectAnswer(e.target.value)} />
-                </div>
-                <div>
-                    <label>Option 1:</label>
-                    <input type="text" value={option1} onChange={(e) => setOption1(e.target.value)} />
-                </div>
-                <div>
-                    <label>Option 2:</label>
-                    <input type="text" value={option2} onChange={(e) => setOption2(e.target.value)} />
-                </div>
-                <div>
-                    <label>Option 3:</label>
-                    <input type="text" value={option3} onChange={(e) => setOption3(e.target.value)} />
-                </div>
-                <button type="submit">Save Question</button>
-            </form>
-        </div>
-    );
-}
+    const handleSearch = async () => {
+            try {
+                const response = await fetch(`http://localhost:8080/users/${searchTerm}/questions`);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setSearchResults(data);
+            } catch (error) {
+                console.error('Error fetching questions:', error);
+                alert('Error fetching questions.');
+            }
+        };
 
-export default QuestionForm;
+        return (
+                <div>
+                    <h2>Create a New Question</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Username:</label>
+                            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        </div>
+                        <div>
+                            <label>Question:</label>
+                            <input type="text" value={questionText} onChange={(e) => setQuestionText(e.target.value)} />
+                        </div>
+                        <div>
+                            <label>Correct Answer:</label>
+                            <input type="text" value={correctAnswer} onChange={(e) => setCorrectAnswer(e.target.value)} />
+                        </div>
+                        <div>
+                            <label>Option 1:</label>
+                            <input type="text" value={option1} onChange={(e) => setOption1(e.target.value)} />
+                        </div>
+                        <div>
+                            <label>Option 2:</label>
+                            <input type="text" value={option2} onChange={(e) => setOption2(e.target.value)} />
+                        </div>
+                        <div>
+                            <label>Option 3:</label>
+                            <input type="text" value={option3} onChange={(e) => setOption3(e.target.value)} />
+                        </div>
+                        <button type="submit">Save Question</button>
+                    </form>
+
+                    <div>
+                        <h2>Search Questions by Username</h2>
+                        <input
+                            type="text"
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                            placeholder="Username"
+                        />
+                        <button type="button" onClick={handleSearch}>Search</button>
+
+                        <div>
+                            <h3>Search Results</h3>
+                            {searchResults.map((question, index) => (
+                                <div key={index}>
+                                    <p>{question.text}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+
+        export default QuestionForm;
--- src/main/java/com/example/triviaApplication/repositories/QuestionRepository.java
+++ src/main/java/com/example/triviaApplication/repositories/QuestionRepository.java
@@ -3,5 +3,9 @@ package com.example.triviaApplication.repositories;
 import com.example.triviaApplication.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    // Method to find questions by username
+    List<Question> findQuestionsByUserUsername(String username);
 }
