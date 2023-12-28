8b97ee87893ba9ab84df619e2a5553b8d2ec3490
allenjosh1983 <138170235+allenjosh1983@users.noreply.github.com>
2023-12-27 22:46:02 -0600

    coded api cash and button functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
 function App() {
+  const [quiz, setQuiz] = useState([]);
+  const [options, setOptions] = useState([]);
 
-const[quiz, setQuiz] = useState([]);
-
-useEffect(() => {
- getQuiz();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setTimeout(() => {
+        getQuiz();
+      }, 5000);
+    }
   }, []);
 
-const getQuiz = async () => {
-      try {
-        const response = await fetch('https://opentdb.com/api.php?amount=1');
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.log('Error fetching quiz:', error);
-      }
-    };
+  const getQuiz = async () => {
+    try {
+      const response = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
+      const data = await response.json();
+      setQuiz(data.results[0].question);
+      setOptions(data.results[0].incorrect_answers.concat(data.results[0].correct_answer));
+      console.log(data);
+    } catch (error) {
+      console.log('Error fetching quiz:', error);
+    }
+  };
 
+  const handleGenerateQuiz = (event) => {
+    event.preventDefault();
+    getQuiz();
+  };
 
+  const createMarkup = (text) => ({ __html: text });
 
-return(
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -36,9 +46,19 @@ return(
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-          <p> </p>
+          <p dangerouslySetInnerHTML={createMarkup(quiz)}></p>
         </a>
+        <form>
+          {options.map((option, index) => (
+            <div key={index}>
+              <input type="radio" id={`option${index}`} name="quizOptions" value={option} />
+              <label htmlFor={`option${index}`} dangerouslySetInnerHTML={createMarkup(option)}></label>
+            </div>
+          ))}
+        </form>
+        <div style={{ marginTop: '20px' }}>
+          <button onClick={handleGenerateQuiz}>Generate Quiz</button>
+        </div>
       </header>
     </div>
   );
