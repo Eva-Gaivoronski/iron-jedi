9f025473edce4c468733f2b2b430e788287011e8
allenjosh1983 <138170235+allenjosh1983@users.noreply.github.com>
2023-12-28 12:34:36 -0600

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,50 +2,119 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
-
 function App() {
+  const [quiz, setQuiz] = useState('');
+  const [options, setOptions] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState('');
+  const [result, setResult] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState('');
 
-const[quiz, setQuiz] = useState([]);
+  const incorrectResponses = [
+    'Not quite right. Try again!',
+    'Incorrect. Keep going!',
+    'Nice try, but that\'s not the correct answer.',
+    // Add more custom incorrect responses as needed
+  ];
 
-useEffect(() => {
-//if (typeof window !== 'undefined') {
-   //getQuiz();
-//}
-//async function getQuiz() {
-//const api = await fetch('https://opentdb.com/api.php?amount=1');
-//const apijson =await api.json();
-//setQuiz(apijson);
-//}
-// }, []);
-const fetchQuiz = async () => {
-      try {
-        const response = await fetch('https://opentdb.com/api.php?amount=1');
-        const data = await response.json();
-        console.log(data); // Assuming the API response has a 'results' property
-      } catch (error) {
-        console.log('Error fetching quiz:', error);
-      }
-    };
-
-    fetchQuiz();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setTimeout(() => {
+        getQuiz();
+      }, 5000);
+    }
   }, []);
 
-return(
+  useEffect(() => {
+    // Check and submit the answer whenever selectedAnswer changes
+    handleQuizSubmit();
+  }, [selectedAnswer, correctAnswer]);
+
+  const getQuiz = async () => {
+    try {
+      const response = await fetch('https://opentdb.com/api.php?amount=1&type=multiple');
+      const data = await response.json();
+      setQuiz(data.results[0].question);
+      setCorrectAnswer(data.results[0].correct_answer);
+      const allOptions = data.results[0].incorrect_answers.concat(data.results[0].correct_answer);
+      setOptions(shuffleArray(allOptions));
+      console.log(data);
+    } catch (error) {
+      console.log('Error fetching quiz:', error);
+    }
+  };
+
+  const handleGenerateQuiz = (event) => {
+    event.preventDefault();
+    getQuiz();
+    setSelectedAnswer('');
+    setResult('');
+  };
+
+  const handleAnswerChange = (event) => {
+    setSelectedAnswer(event.target.value);
+  };
+
+  const handleQuizSubmit = () => {
+    if (selectedAnswer === '') {
+      // Check if an answer is selected before displaying the message
+      setResult('~ Choose Wisely ~');
+      return;
+    }
+
+    const isCorrect = selectedAnswer === correctAnswer;
+    if (isCorrect) {
+      setResult('Correct!');
+    } else {
+      // Randomly select an incorrect response
+      const randomIncorrectResponse =
+        incorrectResponses[Math.floor(Math.random() * incorrectResponses.length)];
+      setResult(randomIncorrectResponse);
+    }
+  };
+
+  const shuffleArray = (array) => {
+    // Fisher-Yates shuffle algorithm
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <p> </p>
-        </a>
+        <p></p>
+        <div>
+          <p dangerouslySetInnerHTML={{ __html: quiz }}></p>
+          <form>
+            {options.map((option, index) => (
+              <div key={index}>
+                <input
+                  type="radio"
+                  id={`option${index}`}
+                  name="quizOptions"
+                  value={option}
+                  checked={selectedAnswer === option}
+                  onChange={handleAnswerChange}
+                />
+                <label
+                  htmlFor={`option${index}`}
+                  dangerouslySetInnerHTML={{ __html: option }}
+                ></label>
+              </div>
+            ))}
+          </form>
+          {result && (
+            <div style={{ marginTop: '20px' }}>
+              <p>{result}</p>
+            </div>
+          )}
+          <div style={{ marginTop: '20px' }}>
+            <button onClick={handleGenerateQuiz}>Generate Quiz</button>
+          </div>
+        </div>
       </header>
     </div>
   );
